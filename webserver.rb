c5867a4277265fa271e7144cc17297a03112f2c9
byGeorge <george@aprillemae.com>
2016-04-16 19:07:24 -0600

    Violet helped me figure out how a webserver was supposed to work. She rocks!

--- webserver.rb
+++ webserver.rb
@@ -3,55 +3,83 @@
 require 'socket'
 require 'resolv'
 require 'uri'
+require 'time'
+
+PATH = 'stuff/'
+# ruby magic to make \n into \r\n
+$/ = "\r\n"
 
 class WebServer
 
 	#initialize gets called at WebServer.new
 	# @variable allows the thread to share data
-	def initialize(request, client)
+	def initialize(client)
 
 		#client connection and request(request)
 		@client = client
-		@request = request
-		#assumes size of resource is 0 until told otherwise
-		@size = 0
 	end
 
 	def serve()
-		
-		lfile = client.peeraddr[2] + " " + "[" + Time.now.to_s + "]" + " \"" + request.to_s.strip! + "\" "
-    	lfle += @status + " " + @size
-		Webserver.get_status
-		puts @status
-    	#log the request
-    	log_file(lfile)
+		loop do
+			#will read entire request
+			request = @client.gets("\r\n\r\n")
+			#assumes size of resource is 0 until told otherwise
+			@size = 0
+
+			file_contents = platter(request)
+			@client.puts("HTTP/1.1 200 OK")
+			@client.puts("Last-Modified: #{Time.new.httpdate}")
+			@client.puts("Server: HAL the Confused Space-ship")
+			@client.puts("Content-Type: CONTENT TYPE GOES HERE")
+			@client.puts("Content-Length: #{file_contents.length}")
+			@client.puts("")
+			@client.print(file_contents)
+			#I don't need to close it because I support persistent connections
+			lfile = "#{@client.peeraddr[3]} [#{Time.new.httpdate}] \"#{request.to_s.strip!}\" 200 #{file_contents.size}"
+	    	#log the request
+	    	log_file(lfile)
+	    	#persistent connection
+
+	    	break if request.include?("Connection: close")
+	    end
+	    #close the client. That's important.
+	    @client.close 
+
+	rescue => e
+		p e
+		puts e.backtrace
+		#if e.message == "404" do stuff
+		@client.close
 	end
 
-	def get_status
-		#split the string into substrings
-		@req = @request.split(' ')
+	def platter(request)
+		file_contents = "oops"
+		#split the first line string into substrings
+		@req = request.each_line.first.split(' ')
+		#if there's not three arguments
+		if @req.count != 3
+			raise "400"
 		#if it starts with 'GET'
-		if @req[0] == 'GET'
-			#just a get statement? invalid request [client typed "GET" and nothing else]
-			if @req[1].nil?
-				@status = "400"
+		elsif @req[0] == 'GET' && @req[2] == "HTTP/1.1"
 			# if it's a simple GET request [client typed "GET HTTP/1.1"]
-			elsif @req[1] == "HTTP/1.1"
-				@status = "200"
-			# another way to get an invalid request [Client typed "GET something "]
-			elsif @req[2].nil?
-				@status = "400"
+			if @req[1] == '/'
 			# if it's a GET request with a web address [client typed "GET something HTTP/1.1"]
 			elsif @req[2] == "HTTP/1.1"
-				#check to make sure it's a real web address, and we are assuming it's an ipv4
-				#new IP address (don't throw exception). check if nil return 404 : otherwise 200
-				req[1] =~ Resolv::IPv4::Regex ? @status = "404" : @status = "200"
+				begin
+					file_contents = File.open(PATH + @req[1]) do |stream| 
+						stream.set_encoding('ASCII-8BIT')
+						stream.read
+					end
+				rescue
+					raise "404"
+				end
 			end #end if http/1.1
 		else
 			#otherwise it's not a valid request
-			@status = "400"
+			raise "400"
 		end  #end if get
-	end
+		file_contents
+	end #end get_status
 end
 
 #logger
@@ -62,9 +90,9 @@ def log_file(entry)
 end
 
 #File.open either opens a file or creates one if it doesn't already exist
-#the second parameter, w+, means that it allows read/write access. The first parameter
+#the second parameter, a, means that it allows read/write access. The first parameter
 #is the file name, and could be /home/HAL/quoththewebserv/404.txt (but isn't)
-$log = File.open(('logfile'), "w+")
+$log = File.open(('logfile'), "a")
 
 #start up a server
 server = TCPServer.new 2880 
@@ -72,19 +100,25 @@ server = TCPServer.new 2880
 #run loop that listens for client requests
 loop do
 	#try to accept client
- 	client = server.accept 
+ 	#client = server.accept 
 
  	#get the client request
- 	request = (client.gets) 	
+ 	#request = (client.gets) 
+ 	#addr = client.peeraddr[2]	
 
  	#service client on a new thread
- 	Thread.start(client, request) do |client, request|
-
-    	WebServer.new(client, request).serve()
+ 	Thread.start(server.accept) do |client|
+ 		begin 
+    		WebServer.new(client).serve()
+    	#this is here for error checking, please pay it no mind
+   		rescue => e
+   			#no really... pe sucks
+    		p e
+    		puts e.backtrace
+    	end
     end
 
  	#and close connection
-  	client.close
 end
 
 #don't leave the log file open!
