547663f59325d9ddd2ba3cb8841c8fac8d38f3bd
george@aprillemae.com <george@aprillemae.com>
2016-04-16 20:49:17 -0600

    server part does servy things

--- webserver.rb
+++ webserver.rb
@@ -1,8 +1,6 @@
 #!/usr/bin/env ruby
 
 require 'socket'
-require 'resolv'
-require 'uri'
 require 'time'
 
 PATH = 'stuff/'
@@ -29,35 +27,46 @@ class WebServer
 			file_contents = platter(request)
 			@client.puts("HTTP/1.1 200 OK")
 			@client.puts("Last-Modified: #{Time.new.httpdate}")
-			@client.puts("Server: HAL the Confused Space-ship")
-			@client.puts("Content-Type: CONTENT TYPE GOES HERE")
+			@client.puts("Server: HAL the Confused Spaceship")
+			@client.puts("Content-Type: #{get_content_type(@req[1]) unless @req[1] == '/'}")
 			@client.puts("Content-Length: #{file_contents.length}")
 			@client.puts("")
 			@client.print(file_contents)
 			#I don't need to close it because I support persistent connections
-			lfile = "#{@client.peeraddr[3]} [#{Time.new.httpdate}] \"#{request.to_s.strip!}\" 200 #{file_contents.size}"
+
+			#create log string
+			lfile = "#{@client.peeraddr[3]} [#{Time.new.httpdate}] \"#{@req.join(" ")}\" 200 #{file_contents.size}"
 	    	#log the request
 	    	log_file(lfile)
 	    	#persistent connection
-
 	    	break if request.include?("Connection: close")
 	    end
 	    #close the client. That's important.
 	    @client.close 
-
+	#rescue is one of the ways that Ruby handles exceptions
 	rescue => e
-		p e
-		puts e.backtrace
-		#if e.message == "404" do stuff
+		if e.message == "404" # if it's a 404 error
+			#create log string
+			lfile = "#{@client.peeraddr[3]} [#{Time.new.httpdate}] \"#{@req.join(" ")}\" 404 0"
+	    	#log the request
+	    	log_file(lfile)
+	    elsif e.message == "400" # if it's a 400 error
+	    	
+	    else
+	    	#something else went wrong
+			p e
+			puts e.backtrace
+	    end
 		@client.close
 	end
 
 	def platter(request)
-		file_contents = "oops"
+		file_contents = ""
 		#split the first line string into substrings
 		@req = request.each_line.first.split(' ')
 		#if there's not three arguments
 		if @req.count != 3
+			#if the request wasn't formatted correctly, raise exception 400
 			raise "400"
 		#if it starts with 'GET'
 		elsif @req[0] == 'GET' && @req[2] == "HTTP/1.1"
@@ -67,10 +76,13 @@ class WebServer
 			elsif @req[2] == "HTTP/1.1"
 				begin
 					file_contents = File.open(PATH + @req[1]) do |stream| 
+						#make sure the stream is binary
 						stream.set_encoding('ASCII-8BIT')
+						#the nice thing about ruby is that you know exactly what the next line does
 						stream.read
 					end
 				rescue
+					#if the resource is not found, throw an exception 404
 					raise "404"
 				end
 			end #end if http/1.1
@@ -80,6 +92,23 @@ class WebServer
 		end  #end if get
 		file_contents
 	end #end get_status
+
+	def get_content_type(file)
+		ext = File.extname(file)
+		if ext == ".txt" || ext == ".rtf" || ext == ".doc"
+			type = "text/plain" 
+		elsif ext == ".gif"
+			type = "image/gif" 
+		elsif ext == ".jpg" || ext == ".jpeg"
+			type = "image/jpeg" 
+		elsif ext == ".png"
+			type = "image/png" 
+		elsif ext == ".HTML"
+			type = "text/html" 
+		else
+			type = "unknown"
+		end
+	end
 end
 
 #logger
@@ -99,13 +128,6 @@ server = TCPServer.new 2880
 
 #run loop that listens for client requests
 loop do
-	#try to accept client
- 	#client = server.accept 
-
- 	#get the client request
- 	#request = (client.gets) 
- 	#addr = client.peeraddr[2]	
-
  	#service client on a new thread
  	Thread.start(server.accept) do |client|
  		begin 
@@ -117,8 +139,6 @@ loop do
     		puts e.backtrace
     	end
     end
-
- 	#and close connection
 end
 
 #don't leave the log file open!
